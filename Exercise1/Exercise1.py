786e75a336e2cbd476ec700c736814085c72b5a2
opetry16 <everlands.op@gmail.com>
2024-01-23 20:40:58 +0100

    V1.2

--- Exercise1/Exercise1.py
+++ Exercise1/Exercise1.py
@@ -3,18 +3,21 @@ import matplotlib.pyplot as plt
 
 # Створення графа
 G = nx.Graph()
- 
-# Тут ми додаємо вершини та ребра 
+
+# Тут ми додаємо вершини та початкові ребра
 ports = ["Шанхай", "Сінгапур", "Ціндао"]
 ships = ["VLOC", "Балкер", "Pioneering_Spirit"]
 G.add_nodes_from(ports + ships)
+initial_edges = [("Шанхай", "VLOC"), ("Сінгапур", "VLOC"), ("Сінгапур", "Балкер"), ("Ціндао", "Балкер"), ("Ціндао", "Pioneering_Spirit")]
+G.add_edges_from(initial_edges)
 
-# Додавання ребер 
-G.add_edges_from([("Шанхай", "VLOC"), ("Сінгапур", "VLOC"), ("Сінгапур", "Балкер"), ("Ціндао", "Балкер"), ("Ціндао", "Pioneering_Spirit")])
+# Додавання додаткових ребер або вершин
+additional_edges = [("Шанхай", "Сінгапур"), ("Сінгапур", "Ціндао")]
+G.add_edges_from(additional_edges)
 
 # Візуалізація графа
-pos = nx.spring_layout(G)  # Визначення позицій для кращої візуалізації
-nx.draw(G, pos, with_labels=True, font_weight="bold", node_size=700, node_color="skyblue", font_color="black", font_size=10, edge_color="gray")
+pos = nx.spring_layout(G)
+nx.draw(G, pos, with_labels=True, font_weight="bold", node_size=700, node_color="skyblue", font_color="black", font_size=10, edge_color="gray", connectionstyle="arc3,rad=0.1")
 plt.title("Мережа морських сполучень")
 plt.show()
 
